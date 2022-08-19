cf2fe7a01609b18d9cd6e3c81a76f6cb89e5420e
GoldenMine0502 <ehe123@naver.com>
2022-08-19 18:26:07 +0900

    mvcconfig, 호텔 목록 파일에서 불러오기

--- src/main/java/kr/goldenmine/how_about_there/hotels/HotelDatabase.java
+++ src/main/java/kr/goldenmine/how_about_there/hotels/HotelDatabase.java
@@ -1,7 +1,11 @@
 package kr.goldenmine.how_about_there.hotels;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.stereotype.Service;
 
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.util.List;
 
 @Service("hotelDatabase")
@@ -9,6 +13,9 @@ public class HotelDatabase {
     List<Hotel> hotels;
 
     public HotelDatabase() {
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Hotel>>() {}.getType();
 
+        hotels = gson.fromJson(new InputStreamReader(HotelDatabase.class.getResourceAsStream("/data/hotels.json")), type);
     }
 }
