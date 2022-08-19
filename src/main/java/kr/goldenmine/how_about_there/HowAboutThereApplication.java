4cd56c06dfee532c43d9bec549725ca554272f4e
Taewon Kim <taewonkim@Taewonui-MacBookPro.local>
2022-08-19 17:55:19 +0900

    Initial commit

---
+++ src/main/java/kr/goldenmine/how_about_there/HowAboutThereApplication.java
@@ -0,0 +1,13 @@
+package kr.goldenmine.how_about_there;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowAboutThereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowAboutThereApplication.class, args);
+    }
+
+}
