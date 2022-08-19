a3605a9c4c28fe7fba61afc42d2fb69801077137
hye0e <nanda5050@naver.com>
2022-08-19 19:39:49 +0900

    define hotel

--- src/main/java/kr/goldenmine/how_about_there/hotels/HotelController.java
+++ src/main/java/kr/goldenmine/how_about_there/hotels/HotelController.java
@@ -1,19 +1,21 @@
 package kr.goldenmine.how_about_there.hotels;
 
 import com.google.gson.JsonObject;
-import kr.goldenmine.how_about_there.hotels.HotelDatabase;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 import kr.goldenmine.how_about_there.users.User;
 import kr.goldenmine.how_about_there.users.UserDatabase;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/hotel")
 public class HotelController {
+
     private final HotelDatabase hotelDatabase;
     private final UserDatabase userDatabase;
 
@@ -29,5 +31,12 @@ public class HotelController {
         JsonObject object = new JsonObject();
 
         return object.toString();
+    }
+
+    @GetMapping()
+    public ResponseEntity hotelList() {
+        List<Hotel> hotelList = hotelDatabase.getAllHotel();
+
+        return ResponseEntity.status(200).body(hotelList);
     }
 }
