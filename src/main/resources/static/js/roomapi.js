5a9545d835deaee973a5d427e418655d40b58c8b
GoldenMine0502 <ehe123@naver.com>
2022-08-19 22:42:26 +0900

    호텔 목록, 룸 목록 js 프론트 작업

---
+++ src/main/resources/static/js/roomapi.js
@@ -0,0 +1,42 @@
+const SERVER_URL = "http://localhost:8080";
+
+let getRoomData = () => {
+    let hotelName = localStorage.getItem("hotelName")
+
+    document.getElementsByClassName("text-box")[0].innerHTML = "<h2>" + hotelName + "</h2>";
+
+    axios.get(SERVER_URL + "/hotel/state?hotelName=" + hotelName).then((response) => {
+        console.log(response.data["bookInfoList"])
+
+        let list = response.data["bookInfoList"]
+
+        let totalHTML = ""
+
+        for (const [key, value] of Object.entries(list)) {
+            console.log(key, value);
+
+            let tag = "<div class=\"list\">" +
+                "<div class=\"text\">" +
+                "<h2>" + key + "호</h2>" +
+                "<ul>" +
+                "<li>4인실</li>" +
+                "<li>"+ value["people"] + "</li>" +
+                "</ul>" +
+                "</div>" +
+                "<div class=\"image\">" +
+                "<img src=\"./image/bed.jpg\">" +
+                "</div>" +
+                "</div>"
+
+            totalHTML += tag;
+        }
+
+        document.getElementsByClassName("room-box")[0].innerHTML = totalHTML;
+    }).catch((error) => {
+
+    });
+}
+
+window.onload = () => {
+    getRoomData()
+}
