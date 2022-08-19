84880164cd326e173a16617ad211575a450aea03
hye0e <nanda5050@naver.com>
2022-08-19 19:53:15 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/kr/goldenmine/how_about_there/hotels/HotelController.java

---
+++ src/main/java/kr/goldenmine/how_about_there/book/BookController.java
@@ -0,0 +1,61 @@
+package kr.goldenmine.how_about_there.book;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import kr.goldenmine.how_about_there.hotels.HotelDatabase;
+import kr.goldenmine.how_about_there.users.User;
+import kr.goldenmine.how_about_there.users.UserDatabase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+    public static final int HOTEL_MAX = 4;
+
+    private final HotelDatabase hotelDatabase;
+    private final UserDatabase userDatabase;
+
+    private final HotelBookDatabase hotelBookDatabase;
+
+    private final Gson gson = new Gson();
+
+    public BookController(HotelDatabase hotelDatabase, UserDatabase userDatabase, HotelBookDatabase hotelBookDatabase) {
+        this.hotelDatabase = hotelDatabase;
+        this.userDatabase = userDatabase;
+        this.hotelBookDatabase = hotelBookDatabase;
+    }
+
+    @GetMapping("/state")
+    public ResponseEntity<HotelInfo> getHotelState(String hotelName) {
+        HotelInfo info = hotelBookDatabase.getHotelBooks().computeIfAbsent(hotelName, HotelInfo::new);
+
+        return ResponseEntity.status(HttpStatus.OK).body(info);
+    }
+
+    @PostMapping("/book")
+    public ResponseEntity<HotelInfo> book(String id, String password, String hotelName, int roomId) {
+        Optional<User> user = userDatabase.login(id, password);
+
+        if(user.isPresent()) {
+            HotelInfo info = hotelBookDatabase.getHotelBooks().computeIfAbsent(hotelName, HotelInfo::new);
+
+            HotelBookInfo bookInfo = info.getBookInfoList().computeIfAbsent(roomId, HotelBookInfo::new);
+
+            if(bookInfo.getPeople().size() < HOTEL_MAX) {
+                if(!bookInfo.getPeople().contains(id)) {
+                    bookInfo.getPeople().add(id);
+
+                    return ResponseEntity.status(HttpStatus.OK).body(info);
+                }
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+    }
+}
